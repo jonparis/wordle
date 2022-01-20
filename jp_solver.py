28aacdaaf817f15d7536146f723ac7e58c565345
Jon Paris <jonparis@gmail.com>
2022-01-19 22:38:47 -0800

    moved over my wordle solver. Best so far is 3.6 but needs tunning

---
+++ jp_solver.py
@@ -0,0 +1,209 @@
+import copy
+from abc import ABC, abstractmethod
+from random import choice as random_choice, sample
+from wordle import Game, Guess, WordGuessCharPositionalResult, WORDS
+from wordleTools import WordleTools, KNOWLEDGE
+
+
+class Solver(ABC):
+    def __init__(self, game):
+        self.game = game
+
+    @abstractmethod
+    def make_a_guess(self):
+        """
+        The game will ask the solver to make a guess (string) over and over until it wins or loses.
+        """
+
+        pass
+
+    def on_guess_processed(self, guess):
+        """
+        The game will call a callback on the solver after a guess is processed with the Guess instance.
+        """
+
+        pass
+
+    def on_end(self):
+        """
+        Upon the game ending, the solver will get a callback.
+        """
+
+        pass
+
+
+class RandomSolver(Solver):
+    def make_a_guess(self):
+        return random_choice(WORDS)
+
+
+class CLISolver(Solver):
+    def make_a_guess(self):
+        if self.game.god_mode:
+            return input(
+                f"THE SOLUTION IS {self.game._target_word}. You have as many guesses left as you want.").strip().lower()
+
+        return input(
+            f"{self.game.MAX_NUM_GUESSES - self.game.num_guesses_taken} guesses left. Please make a guess....").strip().lower()
+
+    def on_end(self):
+        if self.game.won:
+            print(
+                f"Congrats! You got the word in {self.game.num_guesses_taken} guesses.")
+        else:
+            print(
+                f"Sorry, you are out of guesses. The correct word was '{self.game._target_word}'")
+
+
+class AbeSolver(Solver):
+    def __init__(self, game):
+        super().__init__(game)
+        self.characters_that_are_in_target_word = set([])
+        self.characters_that_are_not_in_target_word = set([])
+        self.positional_characters = [None for _ in range(len(WORDS[0]))]
+        self.guessed_words = set([])
+
+    def on_guess_processed(self, guess):
+        self.guessed_words.add(guess.guess)
+
+        for i in range(len(guess.guess)):
+            guess_result = guess.guess_result[i]
+            c = guess.guess[i]
+            if guess_result == WordGuessCharPositionalResult.CORRECT:
+                self.characters_that_are_in_target_word.add(guess.guess[i])
+                self.positional_characters[i] = c
+            elif guess_result == WordGuessCharPositionalResult.IN_WORD:
+                self.characters_that_are_in_target_word.add(guess.guess[i])
+            else:
+                self.characters_that_are_not_in_target_word.add(guess.guess[i])
+
+    def make_a_guess(self):
+        # 1. First, we filter words since we might know about positional characters and also
+        #    if certain characters are definitely not in the word.
+        filtered_words = []
+        filter_words_that_dont_have_known_chars = len(
+            self.characters_that_are_in_target_word) > 0
+        for word in WORDS:
+            char_set = {c for c in word}
+            if filter_words_that_dont_have_known_chars:
+                # The word must have every known char in the target word.
+                if len(self.characters_that_are_in_target_word - char_set) > 0:
+                    continue
+
+            # If the word has a wrong positional character, do not add it.
+            # Also, if the word has a char that we know is not in the target word, do not add it.
+            has_wrong_positional_char = False
+            has_character_that_is_not_in_target_word = False
+            for i in range(len(word)):
+                c = word[i]
+
+                if self.positional_characters[i] is not None and c != self.positional_characters[i]:
+                    has_wrong_positional_char = True
+                    break
+
+                if c in self.characters_that_are_not_in_target_word:
+                    has_character_that_is_not_in_target_word = True
+                    break
+
+            if has_wrong_positional_char or has_character_that_is_not_in_target_word:
+                continue
+
+            filtered_words.append(word)
+
+        # 2. Calculate the adjusted frequency of every letter within filtered_words.
+        denominator = 0.0
+
+        for word in filtered_words:
+            for c in word:
+                denominator += 1.0
+
+        count_map = {}
+        for word in filtered_words:
+            for c in word:
+                if c not in count_map:
+                    count_map[c] = 0.0
+                count_map[c] += 1.0
+
+        # A map of position to a map of character to frequency.
+        frequency_map = {}
+        for word in filtered_words:
+            for c in word:
+                if c not in frequency_map:
+                    frequency_map[c] = 0.0
+
+                frequency_map[c] = count_map[c] / denominator if denominator > 0.0 else 0.0
+
+        # Now, go through every word and calculate the "coverage". Give back the word with the max.
+        ret = None
+        max_coverage = -1.0
+        for word in filtered_words:
+            if word in self.guessed_words:
+                continue
+
+            coverage = sum([frequency_map[c] for c in set([c for c in word])])
+            if coverage > max_coverage:
+                ret = word
+                max_coverage = coverage
+
+        return ret
+
+
+class JPSolver(Solver):
+    def __init__(self, game):
+        super().__init__(game)
+        self.knowledge = WordleTools.default_knowledge()
+
+    def on_guess_processed(self, guess):
+        knowledge = self.knowledge
+
+        for i in range(len(guess.guess)):
+            c = guess.guess[i]
+            guess_result = guess.guess_result[i]
+            if guess_result == WordGuessCharPositionalResult.CORRECT:
+                if c not in knowledge[KNOWLEDGE.IN_WORD]:
+                    knowledge[KNOWLEDGE.IN_WORD].append(c)
+                knowledge[str(i)][KNOWLEDGE.IN_POSITION] = c
+            elif guess_result == WordGuessCharPositionalResult.IN_WORD:
+                if c not in knowledge[str(i)][KNOWLEDGE.NOT_IN_POSITION]:
+                    knowledge[str(i)][KNOWLEDGE.NOT_IN_POSITION].append(c)
+                if c not in knowledge[KNOWLEDGE.IN_WORD]:
+                    knowledge[KNOWLEDGE.IN_WORD].append(c)
+            else:
+                if c not in knowledge[KNOWLEDGE.NOT_IN_WORD]:
+                    knowledge[KNOWLEDGE.NOT_IN_WORD].append(c)
+        self.knowledge = knowledge
+
+    def make_a_guess(self):
+        new_guess = WordleTools.get_suggestion(self.knowledge, WORDS)
+        return new_guess
+
+
+def evaluate_solver(solver_klass, k):
+    """
+    Play the game k times. The overall score will simply be the mean score(i) for all k games,
+    where score(i) = num_guesses_taken if won else MAX_NUM_GUESSES + 1 (so lower is better).
+    """
+
+    assert k > 0
+    k = min(len(WORDS), k)
+    shuffled_words = sample(WORDS, k)
+
+    score_sum = 0
+    for i in range(k):
+        print(
+            f"Evaluating solver iteration {i}, target word = {shuffled_words[i]}...")
+        game = Game(target_word=shuffled_words[i])
+        game.play(solver_klass(game))
+        if game.won:
+            print(
+                f"Solver won in {game.num_guesses_taken} guesses. The word was {game._target_word}")
+        else:
+            print(f"Solver lost. The word was {game._target_word}")
+
+        score_sum += game.num_guesses_taken if game.won else game.MAX_NUM_GUESSES + 1
+
+    return float(score_sum) / float(k)
+
+if __name__ == "__main__":
+    # print("ASolver Average Score: " + str(evaluate_solver(AbeSolver, k=1110)))
+    print("JPSolver Average Score" + str(evaluate_solver(JPSolver, k=1000)))
---
+++ wordleTools.py
@@ -0,0 +1,164 @@
+import copy
+import time
+import datetime
+
+
+class KNOWLEDGE:
+    NOT_IN_WORD = "NIW"
+    IN_WORD = "IW"
+    IN_POSITION = "IP"
+    NOT_IN_POSITION = "NIP"
+    WORD_LENGTH = 5
+
+
+class WordleTools:
+    @staticmethod
+    def test_word_for_match(test_word, knowledge):
+        # remove words that include letters known not to be in word
+        for c in knowledge[KNOWLEDGE.IN_WORD]:
+            if c not in test_word:
+                return False
+        for i in range(KNOWLEDGE.WORD_LENGTH):
+            c = test_word[i]
+            k = knowledge[str(i)]
+            if c in knowledge[KNOWLEDGE.NOT_IN_WORD]:
+                return False
+            if k[KNOWLEDGE.IN_POSITION] and c != k[KNOWLEDGE.IN_POSITION]:
+                return False
+            if c in k[KNOWLEDGE.NOT_IN_POSITION]:
+                return False
+        return True
+
+    @staticmethod
+    def get_possible_matches(knowledge, possible_words):
+        matches = []
+        for word in possible_words:
+            if WordleTools.test_word_for_match(word, knowledge):
+                matches.append(word)
+        return matches
+
+    @staticmethod
+    def get_suggestion(knowledge, guess_options):
+        exclusions_by_guess = {}
+        matches = WordleTools.get_possible_matches(copy.deepcopy(knowledge), guess_options)
+        total_matches = len(matches)
+        # once narrow ensure guess has a chance of being in the set.
+        if total_matches < 5:
+            guess_options = matches
+
+        guess_ct = len(guess_options)
+        search_scale = total_matches * total_matches * guess_ct
+        count = 0
+        start_time = time.time()
+        fast_suggest = WordleTools.get_suggestion_fast(knowledge, matches, guess_options)
+        if search_scale > 100000:
+            return fast_suggest
+
+        for guess in guess_options:
+            exclusions_by_guess[guess] = 0.0
+
+        for secret in matches:
+            # count = WordleTools.status_time_estimate(count, start_time, total_matches)
+
+            for guess in guess_options:
+                for answer in matches:
+                    test_knowledge = WordleTools.update_knowledge(copy.deepcopy(knowledge), secret, guess)
+                    if not WordleTools.test_word_for_match(answer, test_knowledge):
+                        exclusions_by_guess[guess] += 1.0
+        # sort
+        suggested_guess = fast_suggest
+        max_excl = -1.0
+        for g in exclusions_by_guess:
+            if exclusions_by_guess[g] > max_excl:
+                max_excl = exclusions_by_guess[g]
+                suggested_guess = g
+
+        return suggested_guess
+
+    @staticmethod
+    def get_suggestion_fast(knowledge, matches, guess_options):
+        # get as much insight into the letters we don't know about that are in the remaining words
+        # exclude words that
+        letter_count = {}
+        focus_letter_count = {}
+        in_word_letters = knowledge[KNOWLEDGE.IN_WORD]
+        total_matches = len(matches)
+
+        for c in "qwertyuiopasdfghjklzxcvbnm":
+            letter_count[c] = 0.0
+            focus_letter_count[c] = 0.0
+        for word in matches:
+            for c in word:
+                letter_count[c] += 1.0
+                if c not in in_word_letters:
+                    focus_letter_count[c] += 1.0
+
+        max_cov = 0.0
+        suggested_guess = None
+        max_focus = 0.0
+        focus_suggested_word = None
+
+        for word in guess_options:
+            coverage = sum([letter_count[c] for c in set([c for c in word])])
+            focus_coverage = sum([focus_letter_count[c] for c in set([c for c in word])])
+            if coverage > max_cov:
+                max_cov = coverage
+                suggested_guess = word
+            if focus_coverage > max_focus:
+                max_focus = focus_coverage
+                focus_suggested_word = word
+
+        # 500 is a guess. might be worth focusing to a lower word count
+        if focus_suggested_word and total_matches > 300:
+            return focus_suggested_word
+        else:
+            return suggested_guess
+
+    @staticmethod
+    def status_time_estimate(count, start_time, total_matches):
+        # Set timer to make sure the process won't take too long
+        time_now = time.time()
+        elapsed_time = int(time_now - start_time)
+        progress = count / total_matches
+        progress_to_finish = 1 - count / total_matches
+        if progress == 0:
+            time_left_str = "TBD"
+        else:
+            time_left = int(elapsed_time * progress_to_finish / progress)
+            time_left_str = str(datetime.timedelta(seconds=time_left))
+
+        print("{0}% done over ~{1}. Estimated {2} time left".format(str(round(progress * 100, 2)),
+                                                                    str(datetime.timedelta(
+                                                                        seconds=elapsed_time)),
+                                                                    time_left_str))
+        count += 1
+        return count
+        # END TIMER CODE
+
+    @staticmethod
+    def update_knowledge(knowledge, secret_word, guess):
+        for i in range(KNOWLEDGE.WORD_LENGTH):
+            k = knowledge[str(i)]
+            c = guess[i]
+            if c == secret_word[i]:
+                if c not in knowledge[KNOWLEDGE.IN_WORD]:
+                    knowledge[KNOWLEDGE.IN_WORD].append(c)
+                k[KNOWLEDGE.IN_POSITION] = c
+            elif c in secret_word:
+                if c not in knowledge[KNOWLEDGE.IN_WORD]:
+                    knowledge[KNOWLEDGE.IN_WORD].append(c)
+                if c not in k[KNOWLEDGE.NOT_IN_POSITION]:
+                    k[KNOWLEDGE.NOT_IN_POSITION].append(c)
+                    # could optimize if also in word, then the match will have two of that letter
+            else:
+                if c not in knowledge[KNOWLEDGE.NOT_IN_WORD] and c not in knowledge[KNOWLEDGE.IN_WORD]:
+                    knowledge[KNOWLEDGE.NOT_IN_WORD].append(c)
+                # could optimize be removing words with 2+, of c if get back not in work when in word
+        return knowledge
+
+    @staticmethod
+    def default_knowledge():
+        knowledge = {KNOWLEDGE.IN_WORD: [], KNOWLEDGE.NOT_IN_WORD: []}
+        for i in range(KNOWLEDGE.WORD_LENGTH):
+            knowledge[str(i)] = {KNOWLEDGE.NOT_IN_POSITION: [], KNOWLEDGE.IN_POSITION: False}
+        return knowledge
